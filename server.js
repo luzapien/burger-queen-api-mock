dee76dbfeae1a5b801a7c69da9360938286e6893
María Rodríguez <lu.rzapien.92@gmail.com>
2023-05-04 14:35:18 -0600

    deploying in vercel

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const jsonServer = require('json-server');
 const auth = require('json-server-auth');
 const middlewares = jsonServer.defaults()
 
+
+
+
 const app = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
