0fbdabd43b2e2347b077b69cb49a4e4d5217c789
María Rodríguez <lu.rzapien.92@gmail.com>
2023-05-04 16:29:55 -0600

    deploying in vercel5

--- server.js
+++ server.js
@@ -1,42 +1,24 @@
-const fs = require('fs');
-const path = require('path');
-const jsonServer = require('json-server');
-const auth = require('json-server-auth');
-const middlewares = jsonServer.defaults()
+// const fs = require('fs');
+// const path = require('path');
+// const jsonServer = require('json-server');
+// const auth = require('json-server-auth');
+// const middlewares = jsonServer.defaults()
 
+// const app = jsonServer.create();
+// const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
-// server.use(middlewares);
-// // Add this before server.use(router)
-// server.use(
-//  // Add custom route here if needed
-//  jsonServer.rewriter({
-//   "/api/*": "/$1",
-//  })
-// );
-// server.use(router);
-// // Listen to port
-// server.listen(3000, () => {
-//  console.log("JSON Server is running");
+// const port = process.env.PORT || 8080;
+
+// const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
+
+// // /!\ Bind the router db to the app
+// app.db = router.db
+
+// // You must apply the auth middleware before the router
+// app.use(middlewares);
+// app.use(rules);
+// app.use(auth);
+// app.use(router);
+// app.listen(port, () => {
+//     console.log(`JSON Server is running in ${port}`);
 // });
-
-// // Export the Server API
-// module.exports = server;
-
-const app = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'db.json'));
-
-const port = process.env.PORT || 8080;
-
-const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
-
-// /!\ Bind the router db to the app
-app.db = router.db
-
-// You must apply the auth middleware before the router
-app.use(middlewares);
-app.use(rules);
-app.use(auth);
-app.use(router);
-app.listen(port, () => {
-    console.log(`JSON Server is running in ${port}`);
-});
