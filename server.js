86e26d57775388dd2a7a3957f5a6d7fa9ef61843
María Rodríguez <lu.rzapien.92@gmail.com>
2023-05-04 14:49:04 -0600

    deploying in vercel4

--- server.js
+++ server.js
@@ -5,38 +5,38 @@ const auth = require('json-server-auth');
 const middlewares = jsonServer.defaults()
 
 
-server.use(middlewares);
-// Add this before server.use(router)
-server.use(
- // Add custom route here if needed
- jsonServer.rewriter({
-  "/api/*": "/$1",
- })
-);
-server.use(router);
-// Listen to port
-server.listen(3000, () => {
- console.log("JSON Server is running");
-});
-
-// Export the Server API
-module.exports = server;
-
-// const app = jsonServer.create();
-// const router = jsonServer.router(path.join(__dirname, 'db.json'));
-
-// const port = process.env.PORT || 8080;
-
-// const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
-
-// // /!\ Bind the router db to the app
-// app.db = router.db
-
-// // You must apply the auth middleware before the router
-// app.use(middlewares);
-// app.use(rules);
-// app.use(auth);
-// app.use(router);
-// app.listen(port, () => {
-//     console.log(`JSON Server is running in ${port}`);
+// server.use(middlewares);
+// // Add this before server.use(router)
+// server.use(
+//  // Add custom route here if needed
+//  jsonServer.rewriter({
+//   "/api/*": "/$1",
+//  })
+// );
+// server.use(router);
+// // Listen to port
+// server.listen(3000, () => {
+//  console.log("JSON Server is running");
 // });
+
+// // Export the Server API
+// module.exports = server;
+
+const app = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+
+const port = process.env.PORT || 8080;
+
+const rules = auth.rewriter(JSON.parse(fs.readFileSync(path.join(__dirname, 'routes.json'))));
+
+// /!\ Bind the router db to the app
+app.db = router.db
+
+// You must apply the auth middleware before the router
+app.use(middlewares);
+app.use(rules);
+app.use(auth);
+app.use(router);
+app.listen(port, () => {
+    console.log(`JSON Server is running in ${port}`);
+});
