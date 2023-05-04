ba06682727168cd99b54d2273022f86eff6222cd
María Rodríguez <lu.rzapien.92@gmail.com>
2023-05-04 14:42:17 -0600

    deploying in vercel

--- server.js
+++ server.js
@@ -7,6 +7,23 @@ const middlewares = jsonServer.defaults()
 
 
 
+server.use(middlewares);
+// Add this before server.use(router)
+server.use(
+ // Add custom route here if needed
+ jsonServer.rewriter({
+  "/api/*": "/$1",
+ })
+);
+server.use(router);
+// Listen to port
+server.listen(3000, () => {
+ console.log("JSON Server is running");
+});
+
+// Export the Server API
+module.exports = server;
+
 const app = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'db.json'));
 
